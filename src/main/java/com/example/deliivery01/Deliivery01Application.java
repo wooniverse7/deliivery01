7003c97b99815ca98c19c1025436b1ceda02fc70
우성일 <wooniverse7@gmail.com>
2023-11-13 16:29:27 +0900

    first commit

---
+++ src/main/java/com/example/deliivery01/Deliivery01Application.java
@@ -0,0 +1,13 @@
+package com.example.deliivery01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deliivery01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Deliivery01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/deliivery01/Deliivery01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deliivery01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deliivery01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
